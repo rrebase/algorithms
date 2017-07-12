26908847c778ea7af596f125116b1464900ca375
rareba <rareba2ttu.ee>
2017-07-13 00:43:05 +0300

    Day 00

---
+++ bonus/dfs_with_adjacency_list.py
@@ -0,0 +1,25 @@
+# adjacency list representation
+graph = [
+    [3, 5],
+    [2, 3, 4],
+    [1, 5],
+    [1, 0],
+    [1],
+    [2, 0]
+]
+
+path, visited = [], []
+
+
+def dfs(node):  # starting node
+    path.append(node)
+    visited.append(node)
+    if node > len(graph) - 1:
+        return
+    for j in graph[node]:
+        if j not in visited:
+            dfs(j)
+    return path
+
+
+print(dfs(0))  # out -> [0, 3, 1, 2, 5, 4]
---
+++ d00_depth-first_search.py
@@ -0,0 +1,25 @@
+# Depth-first search algorithm
+
+# dictionary representations of graph
+graph = {
+    'A': ['Z'],
+    'Z': ['C', 'Y'],
+    'C': ['D', 'F'],
+    'D': ['B'],
+    'F': ['G'],
+}
+
+visited, path = [], []
+
+
+def dfs(node):  # starting node
+    path.append(node)
+    visited.append(node)
+    if node not in graph:
+        return
+    for i in graph[node]:
+        if i not in visited:
+            dfs(i)
+    return path
+
+print(dfs('A'))  # out -> ['A', 'Z', 'C', 'D', 'B', 'F', 'G', 'Y']
