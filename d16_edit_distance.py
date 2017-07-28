dfe7a78938cc458d22fbd4afda3219f24a3ea2ff
rareba <rrebase@gmail.com>
2017-07-29 00:49:13 +0300

    day 16

--- d16_edit_distance.py
+++ d16_edit_distance.py
@@ -1,3 +1,21 @@
 # Edit distance, Levenshtein distance
+# Minimum number of operations
 
-print(edit_distance("hello", "hellof"))  # -> 1
+def edit_distance(s1, s2):
+    """
+    Return the minimum number or operations required to transform one string into the other.
+    Operations - insert, remove and replace.
+    """
+    dist1 = list(range(len(s2) + 1))
+    for i in range(len(s1)):
+        dist2 = [i + 1]
+        for j in range(len(s2)):
+            if s1[i] == s2[j]:
+                dist2.append(dist1[j])
+            else:
+                dist2.append(1 + min((dist1[j], dist1[j + 1], dist2[-1])))
+        dist1 = dist2
+    return dist1[-1]
+
+
+print(edit_distance("Monday", "Tuesday"))  # -> 4
