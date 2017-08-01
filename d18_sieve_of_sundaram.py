6fc4650af83b444d87d4eceb99d3b095f54f1683
rareba <rrebase@gmail.com>
2017-08-01 23:49:00 +0300

    day 18

---
+++ d18_sieve_of_sundaram.py
@@ -0,0 +1,18 @@
+# Sieve of Sundaram
+
+
+def sundaram(n):
+    n_half = (n - 1) // 2
+    arr = list(range(n_half + 1))
+    i = 1
+    while True:
+        step = 2 * i + 1
+        start = i * (step + 1)
+        if start > n_half:
+            break
+        arr[start::step] = (0 for i in range(start, n_half + 1, step))
+        i += 1
+    return [2] + [2 * k + 1 for k in filter(None, arr)]
+
+
+print(sundaram(30))  # -> [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
